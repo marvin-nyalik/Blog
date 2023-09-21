9c6a9ded63ec43ecf12979b7686e977d53bafb7f
MARVIN <o.silverygold@gmail.com>
2023-09-21 11:56:56 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+  validates :name, presence: true
+  validates :postsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts(user_id)
     user = User.includes(:posts).find(user_id)
