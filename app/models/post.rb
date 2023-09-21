d7a5688ff265cdd955c64a30cc9376e0732740b7
MARVIN <o.silverygold@gmail.com>
2023-09-21 11:56:46 +0300

    Add post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :commentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_user_posts_counter
     user.update(postsCounter: user.posts.count)
