27a3c68ae555d5d3b2a08f3ebc380aa95ded9175
MARVIN <o.silverygold@gmail.com>
2023-09-25 14:07:11 +0300

    Add posts request spec tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users/1/posts' do
+    it 'returns a 200 OK status' do
+      get '/users/1/posts'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get '/users/1/posts'
+      expect(response).to render_template(:index)
+    end
+
+    it 'renders the view with specific HTML text' do
+      get '/users/1/posts'
+      expect(response.body).to include('Heres a list of all posts for a specific user')
+    end
+  end
+
+  describe 'GET /users/1/posts/1' do
+    it 'returns a 200 OK status' do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get '/users/1/posts/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders the view with specific HTML text' do
+      get '/users/1/posts/1'
+      expect(response.body).to include('Here shows the specific post for a user')
+    end
   end
 end
