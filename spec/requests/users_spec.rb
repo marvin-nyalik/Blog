efeab8adc3b790315acc8863db531c9d1d6005c6
MARVIN <o.silverygold@gmail.com>
2023-09-25 14:07:28 +0300

    Add user request spec tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users' do
+    it 'returns a 200 OK status' do
+      get '/users'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+
+    it 'renders the view with specific HTML text' do
+      get '/users'
+      expect(response.body).to include('Here is a list of all users')
+    end
+  end
+
+  describe 'GET /users/1' do
+    it 'returns a 200 OK status' do
+      get '/users/1'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get '/users/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders the view with specific HTML text' do
+      get '/users/1'
+      expect(response.body).to include('A specific user details')
+    end
   end
 end
