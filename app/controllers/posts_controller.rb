22903de8f236169e2c7dc216aeb11dabb27a17c5
MARVIN <o.silverygold@gmail.com>
2023-09-26 09:36:58 +0300

    Update controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
   def index
     # return all posts for a user
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
     # display a specific post
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    @user = User.find(params[:user_id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   def index
     # return all users
+    @users = User.all
   end
 
   def show
     # display a specific user
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 end
