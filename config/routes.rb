a8fb5b587e014c9315056ad1736e2743b8aacf76
MARVIN <o.silverygold@gmail.com>
2023-09-27 16:13:40 +0300

    update routes and styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create, :new] do
+    resources :likes, only: [:create]
+    resources :comments, only: [:create, :new]
   end
+  end
+
 end
