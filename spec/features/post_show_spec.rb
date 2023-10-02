651110e90c480e936d74d8ada95c265ad1cb8d24
MARVIN <o.silverygold@gmail.com>
2023-10-02 14:27:15 +0300

    Fix rubocop errors

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,37 +1,37 @@
 # rubocop:disable Metrics/BlockLength
-require "rails_helper"
-RSpec.feature "Post Show Page" do
+require 'rails_helper'
+RSpec.feature 'Post Show Page' do
   before(:each) do
-    @user1 = create(:user, name: "user1", bio: "Teacher from Uganda", photo: "user1_profile_image.jpg", postsCounter: 0)
-    @user2 = create(:user, name: "User2")
-    @post = create(:post, author_id: @user1.id, title: "Hello", commentsCounter: 2, likesCounter: 1, text: "Post body")
-    @comment = create(:comment, user_id: @user2.id, post_id: @post.id, text: "my comment")
+    @user1 = create(:user, name: 'user1', bio: 'Teacher from Uganda', photo: 'user1_profile_image.jpg', postsCounter: 0)
+    @user2 = create(:user, name: 'User2')
+    @post = create(:post, author_id: @user1.id, title: 'Hello', commentsCounter: 2, likesCounter: 1, text: 'Post body')
+    @comment = create(:comment, user_id: @user2.id, post_id: @post.id, text: 'my comment')
   end
-  scenario "I can see the title of the post" do
+  scenario 'I can see the title of the post' do
     visit user_post_path(@user1, @post)
     expect(page).to have_content(@post.title)
   end
-  scenario "I can see the name of the writer of the post" do
+  scenario 'I can see the name of the writer of the post' do
     visit user_post_path(@user1, @post)
     expect(page).to have_content(@user1.name)
   end
-  scenario "I can see the number of comments of the post" do
+  scenario 'I can see the number of comments of the post' do
     visit user_post_path(@user1, @post)
     expect(page).to have_content(@post.commentsCounter)
   end
-  scenario "I can see the number of likes of the post" do
+  scenario 'I can see the number of likes of the post' do
     visit user_post_path(@user1, @post)
     expect(page).to have_content(@post.likesCounter)
   end
-  scenario "I can see the body of the post" do
+  scenario 'I can see the body of the post' do
     visit user_post_path(@user1, @post)
     expect(page).to have_content(@post.text)
   end
-  scenario "I can see the username of each commenter" do
+  scenario 'I can see the username of each commenter' do
     visit user_post_path(@user1, @post)
     expect(page).to have_content(@user2.name)
   end
-  scenario "I can see the comment of each commenter" do
+  scenario 'I can see the comment of each commenter' do
     visit user_post_path(@user1, @post)
     expect(page).to have_content(@comment.text)
   end
