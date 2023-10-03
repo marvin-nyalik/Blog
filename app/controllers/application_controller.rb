8d46bc508f57395da05c8cde1ee92c9c11d1f879
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-03 16:53:54 +0530

    Modify Application Control with the devise users

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || users_path
+  end
+  private
+  def set_current_user
+    @current_user = current_user
+  end
+end
