578c53a968c4de55239d3766e3ec2ebc718d3b7d
MARVIN <o.silverygold@gmail.com>
2023-10-03 14:20:25 +0300

    Initialize devise:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :new]
   end
   end
-
 end
