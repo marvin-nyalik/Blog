4cf15224903ba2c154f720b09acd685ad24e3cbf
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-03 16:57:12 +0530

    Linters Fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || users_path
   end
+
   private
+
   def set_current_user
     @current_user = current_user
   end
